952e781a96276989c76f33d057c492bdf81671e3
Aldess <106018979+AldessScratch@users.noreply.github.com>
2024-03-12 14:32:25 +0100

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://demo.cloud.rigaprecast.com/v/',
+    bare: 'https://c.info.emersonbrookforest.org/v/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
